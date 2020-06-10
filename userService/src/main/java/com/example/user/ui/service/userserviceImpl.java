545e70849ff375b6f770b545a400aebbd7277525
unknown <hemapriya0440@gmail.com>
2020-06-10 21:24:31 +0530

    h2 added

---
+++ userService/src/main/java/com/example/user/ui/service/userserviceImpl.java
@@ -0,0 +1,13 @@
+package com.example.user.ui.service;
+
+import com.example.user.ui.dto.userdto;
+
+public class userserviceImpl implements userservice {
+
+	@Override
+	public userdto createuser(userdto userdetaiils) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- userService/src/main/java/com/example/userService/controller/userController.java
+++ userService/src/main/java/com/example/userService/controller/userController.java
@@ -1,10 +1,16 @@
 package com.example.userService.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user.ui.model.userRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -15,5 +21,9 @@ public class userController {
 	@GetMapping("status/check")
 	public String staus() {
 		return "running on the port" +env.getProperty("local.server.port");
+	}
+	@PostMapping
+	public String createuser(@Valid @RequestBody userRequestModel userdetails) {
+		return "user created";
 	}
 }
